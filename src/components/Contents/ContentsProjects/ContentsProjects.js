9160db0c99d360d3b150994a1a7a8e3e4da95ae2
Nicole <heeyeons1993@gmail.com>
2020-01-06 18:31:51 -0800

    Working on project section

--- src/components/Contents/ContentsProjects/ContentsProjects.js
+++ src/components/Contents/ContentsProjects/ContentsProjects.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import classes from './ContentsProjects.module.css';
+import ContentProjectItem from './ContentProjectItem/ContentProjectItem';
 
-const contentsProjects = () => {
+const contentsProjects = (props) => {
   return (
-    <div className={classes.Projects}>Projects</div>
+    <div className={classes.Projects}>
+      <div>
+        <h1>Featured Projects</h1>
+      </div>
+
+      <div className={classes.Nav}>
+        <ul>
+          <li><NavLink to='/projects'>All</NavLink></li>
+          <li><NavLink to='/projects/js-react'>JS & React.js</NavLink></li>
+          <li><NavLink to='projects/html-css'>Html & CSS</NavLink></li>
+        </ul>
+      </div>
+
+      <Switch>
+        <Route path='/projects' exact component={ContentProjectItem}/>
+        <Route path='/projects/js-react'/>
+        <Route path='/projects/html-css' />
+      </Switch>
+    </div>
   )
 };
 
