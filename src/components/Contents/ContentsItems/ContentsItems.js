c273fea046c0add5641ab04e2f36731c5ba2a9a1
Nicole <heeyeons1993@gmail.com>
2020-01-10 17:49:52 -0800

    Modified tablet size app

--- src/components/Contents/ContentsItems/ContentsItems.js
+++ src/components/Contents/ContentsItems/ContentsItems.js
@@ -8,14 +8,16 @@ import MainPic from '../../../assets/portpic.jpg';
 const contentsItems = (props) => {
   return (
     <Aux>
-        <h1 className={classes.Name}>Nicole Heeyeon Lee</h1>
-        <div className={classes.Introduction}>
-          <div>
-            <p>Hi, my name is Nicole Lee and I'm a web developer specialised in frontend development. Welcome to my personal website! My final goal is to be a full stack developer.</p>
-            <img src={MainPic} alt='desk'/>
-          </div>
-        </div>
+      <h1 className={classes.Name}>Nicole Heeyeon Lee</h1>
+      <div className={classes.Introduction}>
+      <div>
+        <p>Hi, my name is Nicole Lee and I'm a web developer specialised in frontend development. Welcome to my personal website! My final goal is to be a full stack developer.</p>
+        <img src={MainPic} alt='desk'/>
+      </div>
+      </div>
+      <div className={classes.Container}>
         <Skills />
+      </div>
     </Aux>
   );
 };
