cf3a6222fc678c3fdaefdc4d7ab17e4f8e451b8f
Nicole <heeyeons1993@gmail.com>
2020-01-13 15:45:07 -0800

    Updates

---
+++ precache-manifest.6f13c9af271a95b89bcc8e3cc0e21c99.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c347668d3683e2aa7a20dc6898d18ac8",
+    "url": "/nicole-portfolio/index.html"
+  },
+  {
+    "revision": "b3861b7c3a8e1b45c3f4",
+    "url": "/nicole-portfolio/static/css/main.3d6d0009.chunk.css"
+  },
+  {
+    "revision": "690879e9b8f1c6a3189d",
+    "url": "/nicole-portfolio/static/js/2.58e65a19.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/nicole-portfolio/static/js/2.58e65a19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3861b7c3a8e1b45c3f4",
+    "url": "/nicole-portfolio/static/js/main.b508fc86.chunk.js"
+  },
+  {
+    "revision": "aede768840b83a3c3583",
+    "url": "/nicole-portfolio/static/js/runtime-main.7a9d0ca3.js"
+  },
+  {
+    "revision": "80e6c4ecff0496d99121c73874d836d8",
+    "url": "/nicole-portfolio/static/media/budget.80e6c4ec.PNG"
+  },
+  {
+    "revision": "cc0e3faac44e9a0f535408caa30655f8",
+    "url": "/nicole-portfolio/static/media/css-flex-pannel.cc0e3faa.PNG"
+  },
+  {
+    "revision": "bbbdbc43a6965ae6e5f14909277e328a",
+    "url": "/nicole-portfolio/static/media/js-game.bbbdbc43.PNG"
+  },
+  {
+    "revision": "087438cca0e507a34de8f07152c184f7",
+    "url": "/nicole-portfolio/static/media/paint.087438cc.jpg"
+  },
+  {
+    "revision": "a42be7b681d5805448a6c8b161ac421b",
+    "url": "/nicole-portfolio/static/media/pig-game.a42be7b6.PNG"
+  },
+  {
+    "revision": "21cc1bf0ff22f54cfe781e0a633b8b73",
+    "url": "/nicole-portfolio/static/media/portpic.21cc1bf0.jpg"
+  },
+  {
+    "revision": "a34e2219a88ad99a287e88e08e0c6e22",
+    "url": "/nicole-portfolio/static/media/profile2.a34e2219.png"
+  },
+  {
+    "revision": "78d485f770c0aa3d3edefead6252c0dc",
+    "url": "/nicole-portfolio/static/media/sass-animated.78d485f7.PNG"
+  },
+  {
+    "revision": "acb676fed23b33743592db6cc7e4cbb6",
+    "url": "/nicole-portfolio/static/media/travel-site.acb676fe.PNG"
+  }
+]);
