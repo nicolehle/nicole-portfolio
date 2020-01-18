fe0723c6c436eeeeca2788c352fdf9f0874d6824
Nicole <heeyeons1993@gmail.com>
2020-01-17 18:54:17 -0800

    Modified git icon in profile

--- src/components/Profile/Info/Info.js
+++ src/components/Profile/Info/Info.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FaLinkedinIn, FaGithubAlt, FaCodepen, FaFreeCodeCamp } from 'react-icons/fa';
+import { FaLinkedinIn, FaGit, FaCodepen, FaFreeCodeCamp } from 'react-icons/fa';
 import classes from './Info.module.css';
 import InfoItems from './InfoItems/InfoItems';
 
@@ -9,7 +9,7 @@ const info = () => (
       <h1>Nicole Lee</h1>
         <div className={classes.Flex}>
           <a href='https://linkedin.com/in/nicole-lee-a93b17127' target='_blank' rel='noopener noreferrer'><FaLinkedinIn /></a>
-          <a href='https://github.com/nicolehle' target='_blank' rel='noopener noreferrer'><FaGithubAlt /></a>
+          <a href='https://github.com/nicolehle' target='_blank' rel='noopener noreferrer'><FaGit /></a>
           <a href='https://codepen.io/nicolehle' target='_blank' rel='noopener noreferrer'><FaCodepen /></a>
           <a href='https://www.freecodecamp.org/fcc396720ac-3753-4275-8368-3dc39b02fe5b' target='_blank' rel='noopener noreferrer'><FaFreeCodeCamp/></a>
         </div>
